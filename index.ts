e59c9dd8a0a264b18b22e91be9e96a308c46a760
Park Jin <qkrwls6521@gmail.com>
2023-01-27 20:20:35 +0900

    가제출

--- index.ts
+++ index.ts
@@ -41,9 +41,11 @@ type Customer = {
   string 키 사용 금지.
 */
 
-type Inventory = {};
+type Inventory = {
+  [key in keyof typeof Car]?: keyof typeof Color;
+};
 
-const inventory = {
+const inventory: Inventory = {
   Sedan: "Red",
   Truck: "Black",
 };
@@ -54,9 +56,11 @@ const inventory = {
   모든 키 필수.
 */
 
-type TColor = {};
+type TColor = {
+  [key in keyof typeof Color]: string[]
+};
 
-const colors = {
+const colors: TColor = {
   Red: ["red"],
   Blue: [],
   Black: ["obsidian", "ink"],
@@ -68,7 +72,7 @@ const colors = {
   "Red" | "Blue" | "Black" 사용 금지.
 */
 
-type ColorKey = "";
+type ColorKey = keyof typeof Color;
 
 const someRose: ColorKey = "Red";
 const someSky: ColorKey = "Blue";
@@ -83,7 +87,9 @@ const someTerminal: ColorKey = "Black";
   테스트를 패스 할 함수 로직 작성.
 */
 
-export function getSum(number1, number2) {}
+export function getSum(number1: number, number2: number): number {
+  return number1 + number2
+}
 
 /*
 5. 다음 두 함수를 완성하세요.
@@ -92,9 +98,13 @@ export function getSum(number1, number2) {}
   테스트를 패스 할 함수 로직 작성.
 */
 
-export function isStatusPending(status) {}
+export function isStatusPending(status: Status) {
+  return status === Status.Pending
+}
 
-export function isStatusComplete(status) {}
+export function isStatusComplete(status: Status) {
+  return status === Status.Complete
+}
 
 /*
 6. 다음 함수를 완성하세요
@@ -109,16 +119,23 @@ export function isStatusComplete(status) {}
 */
 
 // 반환 타입
-type StatusObject = {};
+type StatusObject = {
+  [status in keyof typeof Status]: Status
+};
 
-export function getStatusObject() {}
+export function getStatusObject() {
+    return Object.entries(Status).reduce((pre, cur) => ({...pre, [cur[0]]: cur[1].toLowerCase()}), {})
+}
+
 
 /*
 7. 반환 타입을 반환하는 함수를 작성하세요.
   typecasting 사용해보기.
 */
 
-export function getCars(): Car[] {}
+export function getCars(): Car[] {
+  return Object.values(Car) as Car[]
+}
 
 /*
 8. 다음 함수를 완성하세요.
@@ -131,17 +148,32 @@ export function getCars(): Car[] {}
 */
 
 // 반환 타입
-type TProgrammingLanguages = "";
+type TProgrammingLanguages = {
+  [key: number]: ProgrammingLanguage
+}
 
-export function getProgrammingLanguages() {}
+export function getProgrammingLanguages(): TProgrammingLanguages[] {
+  const answer = []
+  for ( const [key, value] of Object.entries(ProgrammingLanguage)) {
+    answer.push({[value.length]: value})
+  }
+  return answer
+}
 
 /*
 9. TOrder를 작성하고 orders 객체에 반영하세요 
 */
 
-type TOrder = {};
+type TOrder = {
+  [key in 'firstCar' | 'secondCar']: {
+    status: Status,
+    color: Color,
+    availableColors: Color[],
+    orderedBy: Customer
+  }
+};
 
-const orders = {
+const orders: TOrder = {
   firstCar: {
     status: Status.Initialized,
     color: Color.Black,
@@ -167,9 +199,15 @@ const orders = {
 string 타입 사용 금지
 */
 
-type TCustomerCar = {};
+type TCustomerCar = {
+  [x: number]: {
+    customerLastName: "skywalker" | "jedi",
+    car: Car,
+    carColor: keyof typeof Color
+  }
+};
 
-const customerCars = {
+const customerCars: TCustomerCar = {
   1: {
     customerLastName: "skywalker",
     car: Car.Coupe,
