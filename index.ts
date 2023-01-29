26559adc501c5876c2310d2fa36b9aa0d0024bd0
Younghwan90s <younghwan1608@gmail.com>
2023-01-29 23:19:40 +0900

    enum exercise_done

--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@
   Do not change
 */
 
+import { isTemplateElement } from "@babel/types";
+import { xdescribe } from "@jest/globals";
+
 // enums
 export enum Status {
   Initialized = "Initialized",
@@ -40,10 +43,11 @@ type Customer = {
   Car enum 사용 필수.
   string 키 사용 금지.
 */
+type Inventory = {
+  [x in keyof typeof Car]?: keyof typeof Color;
+};
 
-type Inventory = {};
-
-const inventory = {
+const inventory: Inventory = {
   Sedan: "Red",
   Truck: "Black",
 };
@@ -54,9 +58,11 @@ const inventory = {
   모든 키 필수.
 */
 
-type TColor = {};
+type TColor = {
+  [x in keyof typeof Color]: string[]
+};
 
-const colors = {
+const colors:TColor = {
   Red: ["red"],
   Blue: [],
   Black: ["obsidian", "ink"],
@@ -68,7 +74,7 @@ const colors = {
   "Red" | "Blue" | "Black" 사용 금지.
 */
 
-type ColorKey = "";
+type ColorKey = keyof typeof Color;
 
 const someRose: ColorKey = "Red";
 const someSky: ColorKey = "Blue";
@@ -83,7 +89,9 @@ const someTerminal: ColorKey = "Black";
   테스트를 패스 할 함수 로직 작성.
 */
 
-export function getSum(number1, number2) {}
+export function getSum(a:number, b:number):number {
+  return a + b
+}
 
 /*
 5. 다음 두 함수를 완성하세요.
@@ -92,9 +100,17 @@ export function getSum(number1, number2) {}
   테스트를 패스 할 함수 로직 작성.
 */
 
-export function isStatusPending(status) {}
+export function isStatusPending(status:Status.Initialized | Status.Pending): boolean | undefined{
+  if (status === "Initialized") {
+    return false
+  } else if (status === "Pending") return true
+}
 
-export function isStatusComplete(status) {}
+export function isStatusComplete(status: Status.Pending | Status.Complete): boolean | undefined {
+  if (status === "Pending") {
+    return false
+  } else if (status === "Complete") return true
+}
 
 /*
 6. 다음 함수를 완성하세요
@@ -109,16 +125,26 @@ export function isStatusComplete(status) {}
 */
 
 // 반환 타입
-type StatusObject = {};
+type StatusObject = {
+  [k in Status]?: keyof typeof Status
+};
 
-export function getStatusObject() {}
+export function getStatusObject(): StatusObject {
+  const result =  Object.entries(Status).reduce((accu, curr) => ({...accu, [curr[0]]: curr[1].toLocaleLowerCase() }), {}
+  );
+  // reduce와 spread operator 같이 쓰면 시간복잡도가 두배 늘어나는데, 본 케이스의 키, 벨류에는 어떻게 다르게 적용하지?
+  return result
+  }
 
 /*
 7. 반환 타입을 반환하는 함수를 작성하세요.
   typecasting 사용해보기.
 */
 
-export function getCars(): Car[] {}
+export function getCars(): Car[] {
+  return Object.values(Car) as Car[]
+
+}
 
 /*
 8. 다음 함수를 완성하세요.
@@ -130,18 +156,35 @@ export function getCars(): Car[] {}
 예: { 10: TypeScript } | key는 value의 length
 */
 
-// 반환 타입
-type TProgrammingLanguages = "";
+// { 10: "TypeScript" },
+// { 10: "JavaScript" },
+// { 6: "Python" },
+// { 6: "Golang" }
 
-export function getProgrammingLanguages() {}
+// 반환 타입
+type TProgrammingLanguages = {
+  [k: number]: keyof typeof ProgrammingLanguage
+};
+
+export function getProgrammingLanguages(): TProgrammingLanguages[] {
+  const result: TProgrammingLanguages[] = Object.entries(ProgrammingLanguage).map((program) => {
+    return { [program[0].length]: program[1]}
+  })
+  return result
+  }
+
 
 /*
 9. TOrder를 작성하고 orders 객체에 반영하세요 
 */
 
-type TOrder = {};
+type TOrder = {
+  [k: string]: {
+    [x: string]: Status | Color | Color[] | Customer
+  }
+};
 
-const orders = {
+const orders: TOrder = {
   firstCar: {
     status: Status.Initialized,
     color: Color.Black,
@@ -167,9 +210,15 @@ const orders = {
 string 타입 사용 금지
 */
 
-type TCustomerCar = {};
+type TCustomerCar = {
+  [k: number]: {
+    customerLastName: "skywalker" | "jedi"
+    car?: Car
+    carColor?: keyof typeof Color
+  }
+};
 
-const customerCars = {
+const customerCars: TCustomerCar = {
   1: {
     customerLastName: "skywalker",
     car: Car.Coupe,
