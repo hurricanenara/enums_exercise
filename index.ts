6853095e50a2956b7ccefae71c039f569b989fff
SangWooRyu <rookas33@gmail.com>
2023-01-30 12:53:02 +0900

    fisst

--- index.ts
+++ index.ts
@@ -43,7 +43,7 @@ type Customer = {
 
 type Inventory = {};
 
-const inventory = {
+const inventory: Inventory = {
   Sedan: "Red",
   Truck: "Black",
 };
@@ -54,9 +54,9 @@ const inventory = {
   모든 키 필수.
 */
 
-type TColor = {};
+type TColor = { Red: []; Blue: []; Black: [] };
 
-const colors = {
+const colors: TColor = {
   Red: ["red"],
   Blue: [],
   Black: ["obsidian", "ink"],
@@ -68,7 +68,7 @@ const colors = {
   "Red" | "Blue" | "Black" 사용 금지.
 */
 
-type ColorKey = "";
+type ColorKey = string;
 
 const someRose: ColorKey = "Red";
 const someSky: ColorKey = "Blue";
@@ -83,7 +83,10 @@ const someTerminal: ColorKey = "Black";
   테스트를 패스 할 함수 로직 작성.
 */
 
-export function getSum(number1, number2) {}
+export function getSum(number1: number, number2: number): number {
+  const sum: number = number1 + number2;
+  return sum;
+}
 
 /*
 5. 다음 두 함수를 완성하세요.
@@ -92,9 +95,13 @@ export function getSum(number1, number2) {}
   테스트를 패스 할 함수 로직 작성.
 */
 
-export function isStatusPending(status) {}
+export function isStatusPending(status: Status.Pending): string {
+  return status;
+}
 
-export function isStatusComplete(status) {}
+export function isStatusComplete(status: Status.Pending): string {
+  return status;
+}
 
 /*
 6. 다음 함수를 완성하세요
@@ -109,16 +116,23 @@ export function isStatusComplete(status) {}
 */
 
 // 반환 타입
-type StatusObject = {};
+type StatusObject = {
+  [key in Status]: string;
+};
 
-export function getStatusObject() {}
+export function getStatusObject(status: StatusObject) {
+  return status;
+}
 
 /*
 7. 반환 타입을 반환하는 함수를 작성하세요.
   typecasting 사용해보기.
 */
 
-export function getCars(): Car[] {}
+export function getCars(): Car[Color] {
+  const carColor: Color = "Red" as Color;
+  return carColor;
+}
 
 /*
 8. 다음 함수를 완성하세요.
@@ -138,9 +152,22 @@ export function getProgrammingLanguages() {}
 9. TOrder를 작성하고 orders 객체에 반영하세요 
 */
 
-type TOrder = {};
+type TOrder = {
+  firstCar: {
+    status: Status;
+    color: Color;
+    availableColors: Color[];
+    orderedBy: { firstName: string; lastName: string };
+  };
+  secondCar: {
+    status: Status;
+    color: Color;
+    availableColors: Color[];
+    orderedBy: { firstName: string; lastName: string };
+  };
+};
 
-const orders = {
+const orders: TOrder = {
   firstCar: {
     status: Status.Initialized,
     color: Color.Black,
@@ -166,9 +193,20 @@ const orders = {
 string 타입 사용 금지
 */
 
-type TCustomerCar = {};
+type TCustomerCar = {
+  1: {
+    customerLastName: string;
+    car: Car;
+    carColor: string;
+  };
+  2: {
+    customerLastName: string;
+    car: Car;
+    carColor: string;
+  };
+};
 
-const customerCars = {
+const customerCars: TCustomerCar = {
   1: {
     customerLastName: "skywalker",
     car: Car.Coupe,
